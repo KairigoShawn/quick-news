f470939b386c41a66029b4a19013052bde1c7338
KairigoShawn <kairigonjogu@outlook.com>
2021-11-03 00:02:50 +0300

    adds app route

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 from flask import Flask, render_template
 from newsapi import NewsApiClient
+
+app = Flask(__name__)
+#creates route function and renders html template here
+@app.route('/')
+def home():
