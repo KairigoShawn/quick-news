e196ab53911e9ac4d64df326ead518b045711b54
KairigoShawn <kairigonjogu@outlook.com>
2021-11-03 00:03:11 +0300

    adds news api key

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 #creates route function and renders html template here
 @app.route('/')
 def home():
+    #client id and api_key
+    newsapi = NewsApiClient(api_key="c912ed1962e542bb8529a76ce984add9")
+
+    
